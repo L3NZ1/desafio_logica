86bf0640498aa960a9ffe527609397424777d72c
L3NZ1 <eduardo.lfaia@gmail.com>
2025-01-21 18:05:42 -0300

    projeto parcialmente realizado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
-let nome="eduardo";
-console.log ("ola " + nome);
+//
+let nome = "eduardo";
+let nivel =10000;
+
+let ranking;
+
+if (nivel <1000) {
+    ranking="ferro";
+}else if (nivel<=2000) {
+    ranking ="bronze"; 
+}else if (nivel<=5000){
+    ranking ="prata";
+}else if (nivel<=7000){
+    ranking ="ouro";
+}else if (nivel<=8000){
+    ranking ="platina";
+}else if (nivel<=9000){
+    ranking ="ascendente";
+}else
+{ (nivel>=10000)
+    ranking ="radiante"
+    console.log ("o temido "+(nome)+" esta no nivel: "+(nivel)+"chegou no "+(ranking)+
+    "o maior ranking que se pode chegar");
+    ;
+}
+        
+ 
+
+
+
